6b13b9483ca215721cd2cc2957097d60b189b87c
Ruhul Amin <r_amin105@yahoo.com>
2020-08-16 21:45:34 +0600

    Add client agreement

--- server/app/controllers/clients.controller.js
+++ server/app/controllers/clients.controller.js
@@ -42,6 +42,35 @@ const create = async (req, res) => {
   return res.status(status.created).send(successMessage);
 };
 
-const clients = { create, getAllClients };
+/**
+ * get Client agreement
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} clients array
+ */
+const getAgreement = async (req, res) => {
+  const db = makeDb(configuration);
+  try {
+    const rows = await db.query(
+      "SELECT id, contract, created FROM contract WHERE created=(select max(created) from contract)"
+    );
+    const dbResponse = rows[0];
+
+    if (!dbResponse) {
+      errorMessage.error = "Contract Cannot be found";
+      return res.status(status.notfound).send(errorMessage);
+    }
+    successMessage.data = dbResponse;
+    return res.status(status.created).send(successMessage);
+  } catch (err) {
+    // handle the error
+    errorMessage.error = "Operation was not successful for Contract";
+    return res.status(status.error).send(errorMessage);
+  } finally {
+    await db.close();
+  }
+};
+
+const clients = { create, getAllClients, getAgreement };
 
 module.exports = clients;
--- server/app/routes/client.routes.js
+++ server/app/routes/client.routes.js
@@ -8,5 +8,6 @@ const router = express.Router();
 
 router.get("/clients", Client.getAllClients); // This is temporary
 router.post("/client", Client.create);
+router.get("/client/agreement", Client.getAgreement);
 
 module.exports = router;
