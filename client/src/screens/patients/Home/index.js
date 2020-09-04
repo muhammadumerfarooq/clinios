6e77ba7fcd27377fa27c1be509890c18180a1fed
Arslan Sajid <arslansajid951@gmail.com>
2020-09-04 16:24:58 +0500

    Patients home screen layout updated

--- client/src/screens/patients/Home/index.js
+++ client/src/screens/patients/Home/index.js
@@ -1,5 +1,66 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+import Card from "../../../components/common/Card";
 
 export default function Home() {
-  return <h1>Patients/Home</h1>;
+  const classes = useStyles();
+  return (
+    <Grid className={classes.main} container spacing={1}>
+      <Grid item xs={3}>
+        {[...Array(3)].map((e, index) => {
+          return (
+            <Card
+              title="Patient"
+              items={[]}
+              showActions={true}
+            />
+          )
+        })}
+      </Grid>
+      <Grid item xs={3}>
+        {[...Array(3)].map((e, index) => {
+          return (
+            <Card
+              title="Patient"
+              items={[]}
+              showActions={true}
+              primaryButtonText={"New"}
+              secondaryButtonText={"Expand"}
+            />
+          )
+        })}
+      </Grid>
+      <Grid item xs={3}>
+        {[...Array(3)].map((e, index) => {
+          return (
+            <Card
+              title="Patient"
+              items={[]}
+              showActions={true}
+            />
+          )
+        })}
+      </Grid>
+      <Grid item xs={3}>
+        {[...Array(3)].map((e, index) => {
+          return (
+            <Card
+              title="Patient"
+              items={[]}
+              showActions={true}
+            />
+          )
+        })}
+      </Grid>
+    </Grid>
+    
+  )
 }
+
+const useStyles = makeStyles((theme) => ({
+  main: {
+      margin: theme.spacing(1, 0, 1, 0),
+      minHeight: "calc(100vh - 192px)"
+  },
+}))
