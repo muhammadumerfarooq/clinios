cfe2139d8af8d4d6810c173e03af48551e5eacfe
Ruhul Amin <r_amin105@yahoo.com>
2020-09-06 10:26:37 +0600

    Refacor customSnackbar.js

--- client/src/components/common/CustomSnackbar.js
+++ client/src/components/common/CustomSnackbar.js
@@ -1,33 +1,31 @@
 import React from "react";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
-import { connect } from "react-redux";
 import { closeSnackbar } from "../../store/auth/actions";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-const CustomSnackbar = ({ isOpen, onClose, severity, message }) => {
+const CustomSnackbar = (props) => {
+  const dispatch = useDispatch();
+
+  const isOpen = useSelector((state) => state.common.snackbar.isOpen);
+  const message = useSelector((state) => state.common.snackbar.message);
+  const severity = useSelector((state) => state.common.snackbar.severity);
+
+  const handleClose = () => {
+    dispatch(closeSnackbar());
+  };
+
   return (
-    <Snackbar open={isOpen} autoHideDuration={3000} onClose={onClose}>
-      <Alert onClose={onClose} severity={severity || "success"}>
+    <Snackbar open={isOpen} autoHideDuration={3000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity={severity || "success"}>
         {message || "Operation completed!"}
       </Alert>
     </Snackbar>
   );
 };
 
-const mapStateToProps = (state /*, ownProps*/) => {
-  return {
-    isOpen: state.common.snackbar.isOpen,
-    message: state.common.snackbar.message,
-    severity: state.common.snackbar.severity,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => ({
-  onClose: () => dispatch(closeSnackbar()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(CustomSnackbar);
+export default CustomSnackbar;
