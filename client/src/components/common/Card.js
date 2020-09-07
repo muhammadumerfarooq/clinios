e3c5ce256a2d64f602b5b26c5cc3ca394f19fdca
Arslan Sajid <arslansajid951@gmail.com>
2020-09-07 11:13:43 +0500

    Card component updated

--- client/src/components/common/Card.js
+++ client/src/components/common/Card.js
@@ -1,140 +1,133 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import PropTypes from "prop-types";
 import { makeStyles } from '@material-ui/core/styles';
 import { Card, Typography, Grid, ListItem, ListItemText, Button, TextField } from '@material-ui/core';
 import Colors from '../../theme/colors';
 
 const PatientCard = (props) => {
-    const classes = useStyles();
-    const { items, title, showActions, primaryButtonText, secondaryButtonText, icon, showSearch} = props;
+  const classes = useStyles();
+  const { items, title, showActions, primaryButtonText, secondaryButtonText, icon, showSearch } = props;
 
-    return (
-        <>
-            <Card className={classes.root} variant="outlined">
-                <Grid container justify="space-between" alignItems="center" className={classes.titleContainer}>
-                    <Typography className={classes.title}>
-                        {title} &nbsp; &nbsp;
-                    </Typography>
-                    {
-                        !!icon && icon
-                    }
-                    {
-                        !!showSearch && (
-                            <TextField
-                                margin='dense'
-                                variant='outlined'
-                                placeholder="Search ..."
-                                className={classes.searchInput}
-                            />
-                        )
-                    }
-                    {
-                        showActions && (
-                            <Grid>
-                                <Button>{primaryButtonText}</Button>
-                                <Button>{secondaryButtonText}</Button>
-                            </Grid>
-                        )
-                    }
-                </Grid>
-                <Grid className={classes.cardContent}>
-                    {
-                        !!items && items.length ?
-                        items.map((item, index) => {
-                            return (
-                                <ListItem key={index} button>
-                                    {/* <ListItemIcon className={classes.sideIcon}>
+  return (
+    <>
+      <Card className={classes.root} variant="outlined">
+        <Grid container justify="space-between" alignItems="center" className={classes.titleContainer}>
+          <Typography className={classes.title}>
+            {title} &nbsp; &nbsp;
+          </Typography>
+          {
+            !!icon && icon
+          }
+          {
+            !!showSearch && (
+              <TextField
+                margin='dense'
+                variant='outlined'
+                placeholder="Search ..."
+                className={classes.searchInput}
+              />
+            )
+          }
+          {
+            showActions && (
+              <Grid>
+                <Button>{primaryButtonText}</Button>
+                <Button>{secondaryButtonText}</Button>
+              </Grid>
+            )
+          }
+        </Grid>
+        <Grid className={classes.cardContent}>
+          {
+            !!items && items.length ?
+              items.map((item, index) => {
+                return (
+                  <ListItem key={index} button>
+                    {/* <ListItemIcon className={classes.sideIcon}>
                                             <img alt={`${item.icon}_icon`} src={require(`../../assets/icons/${item.icon}.png`)} />
                                         </ListItemIcon> */}
-                                    <ListItemText
-                                        classes={{ primary: classes.text }}
-                                        primary={item.name}
-                                    />
-                                </ListItem>
-                            )
-                        })
-                        :
-                        "Data goes here.."
-                    }
-                </Grid>
-            </Card>
-        </>
-    )
+                    <ListItemText
+                      classes={{ primary: classes.text }}
+                      primary={item.name}
+                    />
+                  </ListItem>
+                )
+              })
+              :
+              "Data goes here.."
+          }
+        </Grid>
+      </Card>
+    </>
+  )
 }
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        minHeight: 150,
-        background: Colors.white,
-        border: '1px solid rgba(38, 38, 38, 0.12)',
-        borderRadius: 4,
-        marginBottom: 10
-    },
-    titleContainer: {
-        padding: '0 0 0 1em',
-        borderBottom: `1px solid ${Colors.border}`,
-        minHeight: 47,
-    },
-    title: {
-        fontWeight: '600',
-        fontSize: '1em'
-    },
-    seeMoreLinks: {
-        cursor: "pointer"
-    },
-    cardContent: {
-        padding: theme.spacing(2),
-    },
-    sideIcon: {
-        minWidth: 35,
-    },
-    profileContainer: {
-        padding: theme.spacing(1, 2),
-        cursor: "pointer",
-    },
-    avatar: {
-        marginRight: 15,
-        width: theme.spacing(6),
-        height: theme.spacing(6),
-    },
-    text: {
-        fontStyle: 'normal',
-        fontWeight: '500',
-        fontSize: '1rem',
-        lineHeight: '1.3rem',
-        color: Colors.black
-    },
-    searchInput: {
-        margin: "4px 0"
-    }
+  root: {
+    minHeight: 150,
+    background: Colors.white,
+    border: '1px solid rgba(38, 38, 38, 0.12)',
+    borderRadius: 4,
+    marginBottom: 10
+  },
+  titleContainer: {
+    padding: '0 0 0 1em',
+    borderBottom: `1px solid ${Colors.border}`,
+    minHeight: 47,
+  },
+  title: {
+    fontWeight: '600',
+    fontSize: '1em'
+  },
+  seeMoreLinks: {
+    cursor: "pointer"
+  },
+  cardContent: {
+    padding: theme.spacing(2),
+  },
+  sideIcon: {
+    minWidth: 35,
+  },
+  profileContainer: {
+    padding: theme.spacing(1, 2),
+    cursor: "pointer",
+  },
+  avatar: {
+    marginRight: 15,
+    width: theme.spacing(6),
+    height: theme.spacing(6),
+  },
+  text: {
+    fontStyle: 'normal',
+    fontWeight: '500',
+    fontSize: '1rem',
+    lineHeight: '1.3rem',
+    color: Colors.black
+  },
+  searchInput: {
+    margin: "4px 0"
+  }
 }))
 
 PatientCard.defaultProps = {
-    title: 'Title',
-    showActions: false,
-    showSearch: false,
-    items: [],
-    primaryButtonText: 'History',
-    secondaryButtonText: 'Edit',
-    icon: null,
+  title: 'Title',
+  showActions: false,
+  showSearch: false,
+  items: [],
+  primaryButtonText: 'History',
+  secondaryButtonText: 'Edit',
+  icon: null,
 };
 
 PatientCard.propTypes = {
-    title: PropTypes.string,
-    showActions: PropTypes.bool.isRequired,
-    showSearch: PropTypes.bool.isRequired,
-    items: PropTypes.array.isRequired,
-    primaryButtonText: PropTypes.string,
-    secondaryButtonText: PropTypes.string,
-    icon: PropTypes.node
-  };
+  title: PropTypes.string,
+  showActions: PropTypes.bool.isRequired,
+  showSearch: PropTypes.bool.isRequired,
+  items: PropTypes.array.isRequired,
+  primaryButtonText: PropTypes.string,
+  secondaryButtonText: PropTypes.string,
+  icon: PropTypes.node
+};
 
-function mapStateToProps(state) {
-    return {
-        // user: state.user,
-    };
-  }
-  
 
-export default connect(mapStateToProps)(PatientCard)
+export default PatientCard;
