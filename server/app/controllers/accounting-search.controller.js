fae0c676d2cde3388b0a5f4df2bcfbf0f63589eb
Ruhul Amin <r_amin105@yahoo.com>
2020-09-07 18:10:26 +0600

    Add API Accounting search end point

--- server/app/controllers/accounting-search.controller.js
+++ server/app/controllers/accounting-search.controller.js
@@ -34,8 +34,63 @@ const getAll = async (req, res) => {
   }
 };
 
+/**
+ * @param {object} req
+ * @param {object} res
+ * @returns {object}
+ */
+const search = async (req, res) => {
+  const db = makeDb(configuration, res);
+  const { amount1, amount2 } = req.body.data;
+  let $sql;
+
+  try {
+    if (amount1 && amount2) {
+      $sql = `select t.dt, tt.name, t.amount, e.title encounter_title, t.cpt_id, c.name cpt_name, t.note, t.patient_id
+        , concat(u.firstname, ' ', u.lastname) patient_name, t.created, t.client_id
+        from tran t
+        left join tran_type tt on tt.id=t.type_id
+        left join user u on u.id=t.patient_id
+        left join cpt c on c.id=t.cpt_id
+        left join encounter e on e.id=t.encounter_id
+        where t.client_id=1    
+          and t.amount >= ${amount1}
+          and t.amount <= ${amount2}
+        order by t.dt desc
+        limit 100
+      `;
+    } else {
+      $sql = `select t.dt, tt.name, t.amount, e.title encounter_title, t.cpt_id, c.name cpt_name, t.note, t.patient_id
+        , concat(u.firstname, ' ', u.lastname) patient_name, t.created, t.client_id
+        from tran t
+        left join tran_type tt on tt.id=t.type_id
+        left join user u on u.id=t.patient_id
+        left join cpt c on c.id=t.cpt_id
+        left join encounter e on e.id=t.encounter_id
+        where t.client_id=1    
+        order by t.dt desc
+        limit 100
+      `;
+    }
+    const dbResponse = await db.query($sql);
+    if (!dbResponse) {
+      errorMessage.error = "None found";
+      return res.status(status.notfound).send(errorMessage);
+    }
+    successMessage.data = dbResponse;
+    return res.status(status.created).send(successMessage);
+  } catch (err) {
+    console.log("err", err);
+    errorMessage.error = "Select not successful";
+    return res.status(status.error).send(errorMessage);
+  } finally {
+    await db.close();
+  }
+};
+
 const appointmentTypes = {
   getAll,
+  search,
 };
 
 module.exports = appointmentTypes;
--- server/app/routes/accounting-search.routes.js
+++ server/app/routes/accounting-search.routes.js
@@ -6,6 +6,7 @@ const Client = require("../controllers/accounting-search.controller.js");
 const router = express.Router();
 
 // clients Routes
-router.get("/client/accounting-search", [authJwt.verifyToken], Client.getAll);
+router.get("/client/accounting", [authJwt.verifyToken], Client.getAll);
+router.get("/client/accounting/search", [authJwt.verifyToken], Client.search);
 
 module.exports = router;
