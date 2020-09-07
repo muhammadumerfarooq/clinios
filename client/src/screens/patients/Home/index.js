ac4a45206b8b29cba6a87c2bbcedc0a850740480
Arslan Sajid <arslansajid951@gmail.com>
2020-09-07 11:14:24 +0500

    first column patient card

--- client/src/screens/patients/Home/index.js
+++ client/src/screens/patients/Home/index.js
@@ -4,19 +4,21 @@ import { Grid } from '@material-ui/core';
 import Card from "../../../components/common/Card";
 import CreditCardIcon from '@material-ui/icons/CreditCard';
 import DvrIcon from '@material-ui/icons/Dvr';
+import { FirstColumnPatientCards } from "../../../static/patient"
 
 export default function Home() {
   const classes = useStyles();
   return (
     <Grid className={classes.main} container spacing={1}>
       <Grid item md={3} sm={6} xs={12}>
-        {[...Array(5)].map((e, index) => {
+        {FirstColumnPatientCards.map((item, index) => {
           return (
             <Card
-              title="Patient"
-              items={[]}
-              showActions={true}
-              icon={<DvrIcon />}
+              key={index}
+              title={item.title}
+              items={item.data}
+              showActions={item.showActions}
+              icon={item.icon}
             />
           )
         })}
