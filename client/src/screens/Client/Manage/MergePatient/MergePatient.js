9e9ec0664ee4c6654ccc1560514012ad035adc95
Shimul <theshimulh@gmail.com>
2020-09-08 12:47:10 +0600

    minor change

--- client/src/screens/Client/Manage/MergePatient/MergePatient.js
+++ client/src/screens/Client/Manage/MergePatient/MergePatient.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core";
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    padding: "40px 0px",
+    padding: '40px 0px',
   },
   title: {
     paddingBottom: theme.spacing(1),
@@ -23,9 +23,6 @@ export default function MergePatient() {
         className={classes.title}
       >
         Merge Patient
-      </Typography>
-      <Typography component="p" variant="body2" color="textPrimary">
-        This page is to merger patient account together
       </Typography>
     </div>
   );
