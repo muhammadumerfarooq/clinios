fca7b81704750ff12307891a8522e08fffda700f
Arslan Sajid <arslansajid951@gmail.com>
2020-09-10 23:25:43 +0500

    Added dialogs for first row cards

--- client/src/screens/Patient/Home/index.js
+++ client/src/screens/Patient/Home/index.js
@@ -8,10 +8,38 @@ import BasicInfo from "../BasicInfo"
 
 export default function Home() {
   const classes = useStyles();
-  const [showPatientDialog, setShowPatientDialog] = useState(false);
+  const [showPatientInfoDialog, setShowPatientInfoDialog] = useState(false);
+  const [showPatientHistoryDialog, setShowPatientHistoryDialog] = useState(false);
+
+  const [showAdminInfoDialog, setShowAdminInfoDialog] = useState(false);
+  const [showAdminHistoryDialog, setShowAdminHistoryDialog] = useState(false);
+
+  const [showFormsExpandDialog, setShowFormsExpandDialog] = useState(false);
 
   const togglePatientInfoDialog = () => {
-    setShowPatientDialog(prevState => !prevState)
+    setShowPatientInfoDialog(prevState => !prevState)
+  }
+
+  const togglePatientHistoryDialog = () => {
+    setShowPatientHistoryDialog(prevState => !prevState)
+  }
+
+  const toggleAdminHistoryDialog = () => {
+    setShowAdminHistoryDialog(prevState => !prevState)
+  }
+
+  const toggleFormsExpandDialog = () => {
+    setShowFormsExpandDialog(prevState => !prevState)
+  }
+
+  const mapPrimaryButtonHandlers = (value) => {
+    if(value === 'Patient') {
+      return togglePatientHistoryDialog;
+    } else if(value === 'Admin Notes') {
+      return toggleAdminHistoryDialog;
+    } else if(value === 'Forms') {
+      return toggleFormsExpandDialog;
+    }
   }
 
   const mapSecondaryButtonHandlers = (value) => {
@@ -23,13 +51,40 @@ export default function Home() {
   return (
     <>
     <Dialog
-      open={showPatientDialog}
+      open={showPatientInfoDialog}
       title={"Patient Info"}
       message={<BasicInfo />}
       applyForm={() => togglePatientInfoDialog()}
       cancelForm={() => togglePatientInfoDialog()}
       hideActions={true}
       size={"lg"}
+    />
+    <Dialog
+      open={showPatientHistoryDialog}
+      title={"Patient History"}
+      message={<h3>History</h3>}
+      applyForm={() => togglePatientHistoryDialog()}
+      cancelForm={() => togglePatientHistoryDialog()}
+      hideActions={true}
+      size={"md"}
+    />
+    <Dialog
+      open={showAdminHistoryDialog}
+      title={"Admin Notes History"}
+      message={<h3>Admin Notes History</h3>}
+      applyForm={() => toggleAdminHistoryDialog()}
+      cancelForm={() => toggleAdminHistoryDialog()}
+      hideActions={true}
+      size={"md"}
+    />
+    <Dialog
+      open={showFormsExpandDialog}
+      title={"Forms Expand"}
+      message={<h3>Forms Expand</h3>}
+      applyForm={() => toggleFormsExpandDialog()}
+      cancelForm={() => toggleFormsExpandDialog()}
+      hideActions={true}
+      size={"md"}
     />
     <Grid className={classes.main} container spacing={1}>
       <Grid item md={3} sm={6} xs={12}>
@@ -44,7 +99,7 @@ export default function Home() {
               icon={item.icon}
               primaryButtonText={item.primaryButtonText}
               secondaryButtonText={item.secondaryButtonText}
-              primaryButtonHandler={togglePatientInfoDialog}
+              primaryButtonHandler={mapPrimaryButtonHandlers(item.title)}
               secondaryButtonHandler={mapSecondaryButtonHandlers(item.title)}
             />
           )
