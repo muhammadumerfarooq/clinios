8d2e7deba6aafdf9f672ab786cd0127da03099fb
Arslan Sajid <arslansajid951@gmail.com>
2020-09-10 22:21:11 +0500

    Patient basic info form

--- client/src/screens/Patient/BasicInfo/index.js
+++ client/src/screens/Patient/BasicInfo/index.js
@@ -1,24 +1,52 @@
 import React, { useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Typography, Card, CardContent, TextField, Button, Select, MenuItem } from '@material-ui/core';
-import { BasicInfoForm } from "../../../static/patientBasicInfoForm"
+import { Grid, Typography, Paper, TextField, Button, MenuItem, Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
+import { BasicInfoForm, InsuranceForm, Pharmacies, PaymentData } from "../../../static/patientBasicInfoForm"
+import CountrySelect from "../../../components/common/CountrySelect"
+import RegionSelect from "../../../components/common/RegionSelect"
 
 export default function BasicInfo() {
   const classes = useStyles();
+  const FirstRow = BasicInfoForm.firstRow;
+  const SecondRow = BasicInfoForm.secondRow;
+  const ThirdRow = BasicInfoForm.thirdRow;
+  
+  const [country, setCountry] = useState('')
+  const [region, setRegion] = useState('')
   const [basicInfo, setBasicInfo] = useState({
     firstName: '',
     middleName: '',
     lastName: '',
-    status: 'active',
-    provider: 'active',
+    status: '',
+    provider: '',
+    homePhone: '',
+    cellPhone: '',
+    workPhone: '',
+    email: '',
+    dob: '',
+    otherPhone: '',
+    phoneNotes: '',
+    gender: '',
+    socialSecurity: '',
+    password: '',
+    city: '',
+    zipPostal: '',
   })
 
   const handleInputChnage = (e) => {
-    const {value, name} = e.target;
+    const { value, name } = e.target;
     setBasicInfo({
       ...basicInfo,
       [name]: value
     })
+  }
+
+  const handleCountryRegion = (identifier, value) => {
+    if(identifier === "country") {
+      setCountry(value);
+    } else if(identifier === "region") {
+      setRegion(value)
+    }
   }
 
   console.log("basic", basicInfo)
@@ -27,12 +55,12 @@ export default function BasicInfo() {
     <>
       <Grid container>
         <Grid item xs={12}>
-          <Card className={classes.root} variant="outlined">
-            <CardContent className={classes.sectionCard}>
+          <Paper className={classes.root} variant="outlined">
+            <Grid className={classes.sectionCard}>
               <Typography variant="h5" color="textPrimary" gutterBottom>Basic Information</Typography>
-              <Grid container spacing={1}>
+              <Grid container spacing={1} className={classes.inputRow}>
                 {
-                  BasicInfoForm.map((item, index) => {
+                  FirstRow.map((item, index) => {
                     return (
                       <Grid key={index} item md={2}>
                         {
@@ -42,17 +70,19 @@ export default function BasicInfo() {
                               label={item.label}
                               name={item.name}
                               id={item.id}
+                              type={item.type}
                               fullWidth
                               onChange={(e) => handleInputChnage(e)}
                             />
                             :
-                            <Select
-                              className={classes.select}
+                            <TextField
+                              // className={classes.select}
+                              select
                               placeholder={item.label}
                               label={item.label}
                               id={item.id}
                               name={item.name}
-                              value={basicInfo.status}
+                              value={basicInfo[item.name]}
                               fullWidth
                               onChange={(e) => handleInputChnage(e)}
                             >
@@ -63,103 +93,271 @@ export default function BasicInfo() {
                                   )
                                 })
                               }
-                            </Select>
+                            </TextField>
                         }
                       </Grid>
                     )
                   })
                 }
               </Grid>
-            </CardContent>
-          </Card>
+              <Grid container spacing={1} className={classes.inputRow}>
+                {
+                  SecondRow.map((item, index) => {
+                    return (
+                      <Grid key={index} item md={2}>
+                        {
+                          item.baseType === "input"
+                            ?
+                            <TextField
+                              label={item.label}
+                              name={item.name}
+                              id={item.id}
+                              type={item.type}
+                              fullWidth
+                              onChange={(e) => handleInputChnage(e)}
+                            />
+                            :
+                            <TextField
+                              // className={classes.select}
+                              select
+                              placeholder={item.label}
+                              label={item.label}
+                              id={item.id}
+                              name={item.name}
+                              value={basicInfo[item.name]}
+                              fullWidth
+                              onChange={(e) => handleInputChnage(e)}
+                            >
+                              {
+                                item.options.map((option, index) => {
+                                  return (
+                                    <MenuItem key={index} value={option.value}>{option.label}</MenuItem>
+                                  )
+                                })
+                              }
+                            </TextField>
+                        }
+                      </Grid>
+                    )
+                  })
+                }
+              </Grid>
+              <Grid container spacing={1} className={classes.inputRow}>
+                {
+                  ThirdRow.map((item, index) => {
+                    return (
+                      <Grid key={index} item md={2}>
+                        {
+                          item.baseType === "input"
+                            ?
+                            <TextField
+                              label={item.label}
+                              name={item.name}
+                              id={item.id}
+                              type={item.type}
+                              fullWidth
+                              onChange={(e) => handleInputChnage(e)}
+                            />
+                            :
+                            <TextField
+                              // className={classes.select}
+                              select
+                              placeholder={item.label}
+                              label={item.label}
+                              id={item.id}
+                              name={item.name}
+                              value={basicInfo[item.name]}
+                              fullWidth
+                              onChange={(e) => handleInputChnage(e)}
+                            >
+                              {
+                                item.options.map((option, index) => {
+                                  return (
+                                    <MenuItem key={index} value={option.value}>{option.label}</MenuItem>
+                                  )
+                                })
+                              }
+                            </TextField>
+                        }
+                      </Grid>
+                    )
+                  })
+                }
+              </Grid>
+              <Grid container spacing={1} alignItems="flex-end">
+                <Grid item md={2}>
+                  <Typography>Last Login: {`Jnauary 1, 2020`}</Typography>
+                </Grid>
+                <Grid item md={2}>
+                  <TextField
+                    label={'Password'}
+                    name={'password'}
+                    id={'password'}
+                    type={'password'}
+                    fullWidth
+                    onChange={(e) => handleInputChnage(e)}
+                  />
+                </Grid>
+                <Grid item md={2}>
+                  <Button variant="outlined">
+                    Send Reset Email
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
         </Grid>
       </Grid>
 
-      <Grid container spacing={1}>
+      <Grid container spacing={1} className={classes.inputRow}>
         <Grid item xs={6}>
-          <Card className={classes.root} variant="outlined">
-            <CardContent className={classes.sectionCard}>
+          <Paper className={classes.root} variant="outlined">
+            <Grid className={classes.halfSectionCard}>
               <Typography variant="h5" color="textPrimary">Home Address</Typography>
               <Grid container spacing={1}>
                 <Grid item lg={12}>
                   <TextField
-                    label="Standard"
+                    label="Address"
+                    name={'address'}
                     fullWidth
+                    onChange={(e) => handleInputChnage(e)}
                   />
                 </Grid>
                 <Grid item lg={12}>
                   <TextField
-                    label="Standard"
+                    label="Address Line 2"
+                    name={'address2'}
                     fullWidth
+                    onChange={(e) => handleInputChnage(e)}
                   />
                 </Grid>
                 <Grid item lg={3}>
                   <TextField
-                    label="Standard"
+                    label="City"
+                    name={'city'}
+                    fullWidth
+                    onChange={(e) => handleInputChnage(e)}
                   />
                 </Grid>
                 <Grid item lg={3}>
                   <TextField
-                    label="Standard"
+                    label="Zip/Postal"
+                    name={'zipPostal'}
+                    fullWidth
+                    onChange={(e) => handleInputChnage(e)}
                   />
                 </Grid>
                 <Grid item lg={3}>
-                  <TextField
-                    label="Standard"
+                  <CountrySelect
+                    id={"country-select"}
+                    error={null}
+                    name={"country-select"}
+                    helperText={""}
+                    placeholder={"Country"}
+                    handleChange={(identifier, value) => handleCountryRegion(identifier, value)}
+                    country={country}
                   />
                 </Grid>
                 <Grid item lg={3}>
-                  <TextField
-                    label="Standard"
+                  <RegionSelect
+                    id={"country-select"}
+                    error={null}
+                    name={"country-select"}
+                    helperText={""}
+                    placeholder={"Region"}
+                    handleChange={(identifier, value) => handleCountryRegion(identifier, value)}
+                    country={country}
+                    region={region}
                   />
                 </Grid>
               </Grid>
-            </CardContent>
-          </Card>
+            </Grid>
+          </Paper>
         </Grid>
         <Grid item xs={6}>
-          <Card className={classes.root} variant="outlined">
-            <CardContent className={classes.sectionCard}>
+          <Paper className={classes.root} variant="outlined">
+            <Grid className={classes.halfSectionCard}>
               <Typography variant="h5" color="textPrimary">Pharmacy</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-
-      <Grid container spacing={1}>
-        <Grid item xs={6}>
-          <Card className={classes.root} variant="outlined">
-            <CardContent className={classes.sectionCard}>
-              <Typography variant="h5" color="textPrimary">Insurance</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card className={classes.root} variant="outlined">
-            <CardContent className={classes.sectionCard}>
-              <Typography variant="h5" color="textPrimary">Payment Methods</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-
-      <Grid container>
-        <Grid item xs={12}>
-          <Card className={classes.root} variant="outlined">
-            <CardContent className={classes.sectionCard}>
-              <Typography variant="h5" color="textPrimary">Insurance</Typography>
-            </CardContent>
-          </Card>
+              <Grid container spacing={1}>
+                {
+                  Pharmacies.map((pharmacy, index) => {
+                    return (
+                      <Grid key={index} item md={4}>
+                        <TextField
+                            label={pharmacy.name}
+                            className={classes.inputTextRow}
+                          />
+                        <Typography>{pharmacy.name}</Typography>
+                        <Typography>{pharmacy.address}</Typography>
+                        <Typography>{pharmacy.phone}</Typography>
+                      </Grid>
+                    )
+                  })
+                }
+                </Grid>
+            </Grid>
+          </Paper>
         </Grid>
       </Grid>
 
       <Grid container>
         <Grid item xs={12}>
-          <Card className={classes.root} variant="outlined">
-            <CardContent className={classes.sectionCard}>
-              <Typography variant="h5" color="textPrimary">Payment Methods</Typography>
-            </CardContent>
-          </Card>
+          <Paper className={classes.root} variant="outlined">
+            <Grid className={classes.sectionCard}>
+              <Typography variant="h5" color="textPrimary">Insurance</Typography>
+              <Grid container spacing={1} className={classes.inputRow}>
+                {
+                  InsuranceForm.map((item, index) => {
+                    return (
+                      <Grid key={index} item md={2}>
+                        <TextField
+                          label={item.label}
+                          name={item.name}
+                          id={item.id}
+                          type={item.type}
+                          fullWidth
+                          onChange={(e) => handleInputChnage(e)}
+                        />
+                      </Grid>
+                    )
+                  })
+                }
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+
+      <Grid container>
+        <Grid item xs={12}>
+          <Paper className={classes.root} variant="outlined">
+            <Grid className={classes.sectionCard}>
+              <Typography variant="h5" color="textPrimary">Payment Methods <span><Button size="small" variant="outlined">New</Button></span></Typography>
+              <Table size="small" className={classes.table} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Type</TableCell>
+                    <TableCell align="center">Last Four</TableCell>
+                    <TableCell align="center">Expires</TableCell>
+                    <TableCell align="center">Actions</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {PaymentData.map((row) => (
+                    <TableRow key={row.type}>
+                      <TableCell component="th" scope="row">{row.type}</TableCell>
+                      <TableCell align="center">{row.lastFour}</TableCell>
+                      <TableCell align="center">{row.expires}</TableCell>
+                      <TableCell align="center">
+                        <Button>Edit</Button>
+                        <Button>Delete</Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Grid>
+          </Paper>
         </Grid>
       </Grid>
 
@@ -172,15 +370,28 @@ export default function BasicInfo() {
 }
 
 const useStyles = makeStyles((theme) => ({
+  inputRow: {
+    marginBottom: theme.spacing(1),
+  },
   sectionCard: {
-    padding: theme.spacing(1),
+    padding: theme.spacing(1.5, 1),
+  },
+  halfSectionCard: {
+    padding: theme.spacing(1.5, 1),
+    minHeight: 198,
   },
   root: {
     border: '1px solid',
     margin: theme.spacing(0, 0, 1, 0),
     borderRadius: 0
   },
+  inputTextRow: {
+    marginBottom: theme.spacing(3),
+  },
   select: {
     lineHeight: '2.30em',
+  },
+  table: {
+    background: 'white'
   }
 }))
