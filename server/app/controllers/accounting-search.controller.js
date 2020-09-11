0d031bbfc7dbe4d4ee631d36b0d9cb19bfcfc38a
David <david10@davidjpotter.com>
2020-09-10 20:48:24 -0700

    .

--- server/app/controllers/accounting-search.controller.js
+++ server/app/controllers/accounting-search.controller.js
@@ -3,19 +3,14 @@
 const { configuration, makeDb } = require("../db/db.js");
 const { errorMessage, successMessage, status } = require("../helpers/status");
 
-/**
- * @param {object} req
- * @param {object} res
- * @returns {object}
- */
 const getAll = async (req, res) => {
   const db = makeDb(configuration, res);
   try {
     const dbResponse = await db.query(
       `select id, name
-            from tran_type tt
-            where (client_id is null or client_id=1)
-            order by 1
+       from tran_type tt
+       where (client_id is null or client_id=1)
+       order by 1
       `
     );
 
@@ -34,44 +29,29 @@ const getAll = async (req, res) => {
   }
 };
 
-/**
- * @param {object} req
- * @param {object} res
- * @returns {object}
- */
 const search = async (req, res) => {
   const db = makeDb(configuration, res);
   const { amount1, amount2 } = req.body.data;
   let $sql;
 
   try {
+    $sql = `select t.dt, tt.name, t.amount, e.title encounter_title, t.cpt_id, c.name cpt_name, t.note, t.patient_id
+      , concat(u.firstname, ' ', u.lastname) patient_name, t.created, t.client_id
+      from tran t
+      left join tran_type tt on tt.id=t.type_id
+      left join user u on u.id=t.patient_id
+      left join cpt c on c.id=t.cpt_id
+      left join encounter e on e.id=t.encounter_id
+      where t.client_id=${req.client_id}    
+    `;
     if (amount1 && amount2) {
-      $sql = `select t.dt, tt.name, t.amount, e.title encounter_title, t.cpt_id, c.name cpt_name, t.note, t.patient_id
-        , concat(u.firstname, ' ', u.lastname) patient_name, t.created, t.client_id
-        from tran t
-        left join tran_type tt on tt.id=t.type_id
-        left join user u on u.id=t.patient_id
-        left join cpt c on c.id=t.cpt_id
-        left join encounter e on e.id=t.encounter_id
-        where t.client_id=1    
-          and t.amount >= ${amount1}
-          and t.amount <= ${amount2}
-        order by t.dt desc
-        limit 100
-      `;
-    } else {
-      $sql = `select t.dt, tt.name, t.amount, e.title encounter_title, t.cpt_id, c.name cpt_name, t.note, t.patient_id
-        , concat(u.firstname, ' ', u.lastname) patient_name, t.created, t.client_id
-        from tran t
-        left join tran_type tt on tt.id=t.type_id
-        left join user u on u.id=t.patient_id
-        left join cpt c on c.id=t.cpt_id
-        left join encounter e on e.id=t.encounter_id
-        where t.client_id=1    
-        order by t.dt desc
-        limit 100
-      `;
+      $sql=$sql+`  and t.amount >= ${amount1}
+      and t.amount <= ${amount2}
+      `
     }
+    $sql=$sql+`order by t.dt desc
+      limit 100
+    `
     const dbResponse = await db.query($sql);
     if (!dbResponse) {
       errorMessage.error = "None found";
