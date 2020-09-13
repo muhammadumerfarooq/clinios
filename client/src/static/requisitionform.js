18b24175cb27ad98eefea457308f3dbe481772bc
Arslan Sajid <arslansajid951@gmail.com>
2020-09-13 17:28:53 +0500

    RequisitionsForm fields

---
+++ client/src/static/requisitionform.js
@@ -0,0 +1,94 @@
+export const BillSelectionFields = [
+  {
+    label: 'Physician',
+    value: 'physician',
+  },
+  {
+    label: 'Patient',
+    value: 'patient',
+  },
+  {
+    label: 'Insurance',
+    value: 'insurance',
+  },
+]
+
+export const LabortoriesSelectionFields = [
+  {
+    label: 'Physician',
+    value: 'physician',
+  },
+  {
+    label: 'Patient',
+    value: 'patient',
+  },
+  {
+    label: 'Insurance',
+    value: 'insurance',
+  },
+  {
+    label: 'Physician',
+    value: 'physician',
+  },
+  {
+    label: 'Patient',
+    value: 'patient',
+  },
+  {
+    label: 'Insurance',
+    value: 'insurance',
+  },
+  {
+    label: 'Physician',
+    value: 'physician',
+  },
+  {
+    label: 'Patient',
+    value: 'patient',
+  },
+  {
+    label: 'Insurance',
+    value: 'insurance',
+  },
+]
+
+
+export const FavoritesSelectionFields = [
+  {
+    label: 'Physician',
+    value: 'physician',
+  },
+  {
+    label: 'Patient',
+    value: 'patient',
+  },
+  {
+    label: 'Insurance',
+    value: 'insurance',
+  },
+  {
+    label: 'Physician',
+    value: 'physician',
+  },
+  {
+    label: 'Patient',
+    value: 'patient',
+  },
+  {
+    label: 'Insurance',
+    value: 'insurance',
+  },
+  {
+    label: 'Physician',
+    value: 'physician',
+  },
+  {
+    label: 'Patient',
+    value: 'patient',
+  },
+  {
+    label: 'Insurance',
+    value: 'insurance',
+  },
+]
+
