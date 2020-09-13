628e62458aad7b34d76cc02a0d5d92742eaccefe
Arslan Sajid <arslansajid951@gmail.com>
2020-09-13 17:27:37 +0500

    updated allergies and new messages dialog

--- client/src/screens/Patient/Messages/NewMessage/index.js
+++ client/src/screens/Patient/Messages/NewMessage/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextField, Button, Grid, Typography, MenuItem } from '@material-ui/core';
+import { TextField, Button, Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const NewMessage = (props) => {
