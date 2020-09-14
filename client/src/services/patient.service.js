1be46c42e205e06fb16fa9ccbeb7ba0ea6515f00
Arslan Sajid <arslansajid951@gmail.com>
2020-09-14 10:36:08 +0500

    patient service added

---
+++ client/src/services/patient.service.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+
+const API_URL =
+  `${process.env.REACT_APP_API_URL}api/v1` || "http://localhost:5000/api/v1";
+
+class Patient {
+  getAllergies() {
+    return axios
+      .get(API_URL + `/patient/allergies`, { headers: authHeader() })
+      .then((res) => res.data);
+  }
+  getBillings() {
+    return axios
+      .get(API_URL + `/patient/billing/?limit=3`, { headers: authHeader() })
+      .then((res) => res.data);
+  }
+  // create(data) {
+  //   return axios.post(API_URL + `/appointments`, data, {
+  //     headers: authHeader(),
+  //   });
+  // }
+  // update(data, userId, appointmentId) {
+  //   return axios.put(
+  //     API_URL + `/appointments/${userId}/${appointmentId}`,
+  //     data,
+  //     {
+  //       headers: authHeader(),
+  //     }
+  //   );
+  // }
+  // deleteById(id) {
+  //   return axios.delete(API_URL + `/appointments/${id}`, {
+  //     headers: authHeader(),
+  //   });
+  // }
+}
+
+export default new Patient();
