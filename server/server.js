cf00848b9504ce137a7618d609789c08e483e290
Ruhul Amin <r_amin105@yahoo.com>
2020-09-14 12:11:26 +0600

    Add process-lab files

--- server/server.js
+++ server/server.js
@@ -38,6 +38,7 @@ app.use("/api/v1", require("./app/routes/patient.routes"));
 app.use("/api/v1", require("./app/routes/email-patient.routes"));
 app.use("/api/v1", require("./app/routes/support.routes"));
 app.use("/api/v1", require("./app/routes/forms.routes"));
+app.use("/api/v1", require("./app/routes/process-lab.routes"));
 
 app.listen(config.port).on("listening", () => {
   console.log(`API is live on ${config.port}`);
