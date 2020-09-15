650223a7e0c1a45339cfdd7e1698c8346770dd22
Arslan Sajid <arslansajid951@gmail.com>
2020-09-15 23:10:18 +0500

    Added card content components

---
+++ client/src/screens/Patient/Requisitions/content.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Grid
+} from "@material-ui/core";
+
+export default function Content(props) {
+  const classes = useStyles();
+  const { data } = props;
+
+  return (
+    <>
+    {
+      data.map(item => (
+        <Grid key={item.id} className={classes.inputRow}>
+          <Grid component="span">
+            {item.name} {item.created}
+          </Grid>
+        </Grid>
+      ))
+    } 
+    </>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  inputRow: {
+    marginBottom: theme.spacing(1),
+  },
+}));
