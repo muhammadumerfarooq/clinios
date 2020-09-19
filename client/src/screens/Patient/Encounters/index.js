4423640e4c4cb82e7c73f3ffc706869a139bcbfc
Arslan Sajid <arslansajid951@gmail.com>
2020-09-19 12:14:29 +0500

    Encounters form component

---
+++ client/src/screens/Patient/Encounters/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import moment from "moment";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Grid,
+  Divider,
+  Typography
+} from "@material-ui/core";
+
+export default function Content(props) {
+  const classes = useStyles();
+  const { data } = props;
+
+  return (
+    <>
+      <Typography variant="h3" color="textSecondary">Encounters</Typography>
+    </>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  inputRow: {
+    marginBottom: theme.spacing(1),
+  },
+}));
