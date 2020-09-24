47f0e6eda278d11cf8b6cbd9c912e18d04f6c7f3
Usman Ali <usman_ali545@yahoo.com>
2020-09-24 11:05:09 +1000

    add backend for integrations

---
+++ server/app/controllers/integrations.controller.js
@@ -0,0 +1,69 @@
+"use strict";
+const { validationResult } = require("express-validator");
+const { configuration, makeDb } = require("../db/db.js");
+const { errorMessage, successMessage, status } = require("../helpers/status");
+
+const getIntegrations = async (req, res) => {
+  const db = makeDb(configuration, res);
+  try {
+    const dbResponse = await db.query(
+      `select id, labcorp_api_key, quest_api_key, doctors_data_username, doctors_data_password, stripe_api_key
+      from client
+      where id=${req.client_id}
+      `
+    );
+
+    if (!dbResponse) {
+      errorMessage.error = "None found";
+      return res.status(status.notfound).send(errorMessage);
+    }
+    successMessage.data = dbResponse;
+    return res.status(status.created).send(successMessage);
+  } catch (err) {
+    errorMessage.error = "Select not successful";
+    return res.status(status.error).send(errorMessage);
+  } finally {
+    await db.close();
+  }
+};
+
+const update = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    errorMessage.error = errors.array();
+    return res.status(status.error).send(errorMessage);
+  }
+
+  const db = makeDb(configuration, res);
+  let client = req.body;
+
+  client.updated = new Date();
+
+  try {
+    const updateResponse = await db.query(
+      `update client set ? where id =${req.client_id}`,
+      [client]
+    );
+
+    if (!updateResponse.affectedRows) {
+      errorMessage.error = "Update not successful";
+      return res.status(status.notfound).send(errorMessage);
+    }
+    successMessage.data = updateResponse;
+    successMessage.message = "Update successful";
+    return res.status(status.success).send(successMessage);
+  } catch (error) {
+    errorMessage.error = "Update not successful";
+    return res.status(status.error).send(errorMessage);
+  } finally {
+    await db.close();
+  }
+};
+
+
+const integrations = {
+  getIntegrations,
+  update,
+};
+
+module.exports = integrations;
