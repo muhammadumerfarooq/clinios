6e940d2602d4d540df16190e3847076f302d6b72
Shimul <theshimulh@gmail.com>
2020-09-25 08:21:14 +0600

    Fixes the column orientation

--- client/src/screens/Client/Manage/Support/Support.js
+++ client/src/screens/Client/Manage/Support/Support.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, withStyles } from '@material-ui/core';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
+import React, { useState } from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, withStyles } from "@material-ui/core";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    padding: '40px 0px',
+    padding: "40px 0px",
   },
   title: {
     paddingBottom: theme.spacing(1),
@@ -27,23 +27,23 @@ const useStyles = makeStyles((theme) => ({
   tableContainer: {
     minWidth: 650,
     marginTop: theme.spacing(2),
-    maxWidth: '70%',
+    maxWidth: "70%",
   },
 
   actions: {
-    textAlign: 'center',
-    display: 'flex',
-    border: 'none',
-    '& button': {
-      fontSize: '12px',
+    textAlign: "center",
+    display: "flex",
+    border: "none",
+    "& button": {
+      fontSize: "12px",
     },
   },
   customSelect: {
-    width: '185px',
+    width: "185px",
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
-    maxWidth: '180px',
+    maxWidth: "180px",
   },
 }));
 
@@ -51,7 +51,7 @@ const StyledTableCell = withStyles((theme) => ({
   head: {
     backgroundColor: theme.palette.grey,
     color: theme.palette.grey,
-    fontSize: '12px',
+    fontSize: "12px",
     fontWeight: 700,
   },
   body: {
@@ -62,13 +62,13 @@ const StyledTableCell = withStyles((theme) => ({
 const StyledTableRow = withStyles((theme) => ({
   root: {
     fontSize: 14,
-    '&:nth-of-type(odd)': {
+    "&:nth-of-type(odd)": {
       backgroundColor: theme.palette.action.hover,
     },
-    '& th': {
+    "& th": {
       fontSize: 12,
     },
-    '& td': {
+    "& td": {
       fontSize: 12,
     },
   },
@@ -76,10 +76,10 @@ const StyledTableRow = withStyles((theme) => ({
 
 export default function Support() {
   const classes = useStyles();
-  const [cases, setCases] = useState('');
+  const [cases, setCases] = useState("");
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
+      <Grid container direction="column" spacing={3}>
         <Grid item xs={12} sm={3}>
           <Typography
             component="h1"
@@ -88,6 +88,9 @@ export default function Support() {
             className={classes.title}
           >
             Technical Support
+          </Typography>
+          <Typography component="p" variant="body2" color="textPrimary">
+            This page is used for support
           </Typography>
         </Grid>
         <Grid item xs={12} sm={2}>
@@ -115,9 +118,6 @@ export default function Support() {
           New Case
         </Button>
       </Grid>
-      <Typography component="p" variant="body2" color="textPrimary">
-        This page is used for support
-      </Typography>
 
       <TableContainer component={Paper} className={classes.tableContainer}>
         <Table className={classes.table} aria-label="a dense table">
