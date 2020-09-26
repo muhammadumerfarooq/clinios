1fbffa7af9a1869c9456c8f9ba9cfeee12ceef54
Shimul <theshimulh@gmail.com>
2020-09-26 09:06:24 +0600

    delets multiple autofocus

--- client/src/screens/Client/Manage/MergePatient/MergePatient.js
+++ client/src/screens/Client/Manage/MergePatient/MergePatient.js
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import React, { useState } from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    padding: '40px 0px',
+    padding: "40px 0px",
   },
   title: {
     paddingBottom: theme.spacing(1),
   },
   mergeItems: {
-    maxWidth: '500px',
-    display: 'flex',
-    flexDirection: 'column',
+    maxWidth: "500px",
+    display: "flex",
+    flexDirection: "column",
   },
   submit: {
-    maxWidth: '100px',
+    maxWidth: "100px",
   },
 }));
 
 export default function MergePatient() {
   const classes = useStyles();
-  const [patientIdKeep, setPatientIdKeep] = useState('');
-  const [patientIdDelete, setPatientIdDelete] = useState('');
+  const [patientIdKeep, setPatientIdKeep] = useState("");
+  const [patientIdDelete, setPatientIdDelete] = useState("");
   return (
     <div className={classes.root}>
       <Typography
@@ -56,7 +56,6 @@ export default function MergePatient() {
           label="Patient Id to Delete"
           name="patientIdDelete"
           autoComplete="patientIdDelete"
-          autoFocus
           onChange={(event) => setPatientIdDelete(event.target.value)}
         />
 
