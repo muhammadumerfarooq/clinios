a0bb6073e2178a38954eeacc5af2fa5854c4f6dc
Ruhul Amin <r_amin105@yahoo.com>
2020-10-04 20:40:37 +0600

    fix email service

--- client/src/services/email.service.js
+++ client/src/services/email.service.js
@@ -3,13 +3,13 @@ import { API_BASE } from "./../utils/API_BASE";
 
 class EmailService {
   sendEmailVerification(user) {
-    return axios.post(API_BASE + `/send/verification/`, user);
+    return axios.post(API_BASE + `/email/send/verification/`, user);
   }
   resendEmailVerification(user) {
-    return axios.post(API_BASE + `/resend/verification/`, user);
+    return axios.post(API_BASE + `/email/resend/verification/`, user);
   }
   emailVerify(userId, token) {
-    return axios.get(API_BASE + `/confirmation/${userId}/${token}`);
+    return axios.get(API_BASE + `/email/confirmation/${userId}/${token}`);
   }
 }
 
