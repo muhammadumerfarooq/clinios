b066b385a328fb72f6ea7abf2b11c4b8b5ed9bb8
Arslan Sajid <arslansajid951@gmail.com>
2020-10-06 15:01:58 +0500

    editor text added to redux for admin/medical notes

--- client/src/store/patient/actions.js
+++ client/src/store/patient/actions.js
@@ -1,6 +1,8 @@
 import {
   SET_SELECTED_ENCOUNTER,
-  RESET_SELECTED_ENCOUNTER
+  RESET_SELECTED_ENCOUNTER,
+  SET_EDITOR_TEXT,
+  RESET_EDITOR_TEXT,
 } from "./types";
 
 
@@ -14,5 +16,19 @@ export const setEncounter = (encounter) => {
 export const resetEncounter = () => {
   return {
     type: RESET_SELECTED_ENCOUNTER
+  };
+};
+
+
+export const setEditorText = (value) => {
+  return {
+    type: SET_EDITOR_TEXT,
+    payload: value,
+  };
+};
+
+export const resetEditorText = () => {
+  return {
+    type: RESET_EDITOR_TEXT
   };
 };
--- client/src/store/patient/reducer.js
+++ client/src/store/patient/reducer.js
@@ -1,10 +1,13 @@
 import {
   SET_SELECTED_ENCOUNTER,
-  RESET_SELECTED_ENCOUNTER
+  RESET_SELECTED_ENCOUNTER,
+  SET_EDITOR_TEXT,
+  RESET_EDITOR_TEXT
 } from "./types";
 
 const initState = {
   selectedEncounter: null,
+  editorText: '',
 };
 
 const reducer = (state = initState, action) => {
@@ -19,6 +22,17 @@ const reducer = (state = initState, action) => {
         ...state,
         selectedEncounter: null
       };
+      case SET_EDITOR_TEXT:
+      return {
+        ...state,
+        editorText: action.payload
+      };
+      case RESET_EDITOR_TEXT:
+      return {
+        ...state,
+        editorText: ''
+      };
+    //editor case
     default:
       return state;
   }
