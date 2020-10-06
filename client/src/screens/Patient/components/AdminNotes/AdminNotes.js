5a54900704af0fa714403c04a6af60c57406b555
Arslan Sajid <arslansajid951@gmail.com>
2020-10-06 15:00:59 +0500

    Admin Notes updated for github issue

--- client/src/screens/Patient/components/AdminNotes/AdminNotes.js
+++ client/src/screens/Patient/components/AdminNotes/AdminNotes.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { TextField, IconButton, Grid } from "@material-ui/core";
+import { TextField, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import PatientService from "./../../../../services/patient.service";
 import { setError, setSuccess } from "./../../../../store/common/actions";
+import { setEditorText, resetEditorText } from "./../../../../store/patient/actions";
 import { useDispatch } from "react-redux";
-
-import SaveIcon from '@material-ui/icons/CheckCircle';
-import CancelIcon from '@material-ui/icons/Cancel';
 
 const AdminNotes = (props) => {
   const dispatch = useDispatch();
@@ -23,6 +21,7 @@ const AdminNotes = (props) => {
       ...formFields,
       [name]: value,
     });
+    dispatch(setEditorText(value));
   };
 
   useEffect(() => {
@@ -81,21 +80,15 @@ const AdminNotes = (props) => {
               fullWidth
               onChange={(e) => handleInputChange(e)}
               multiline={true}
-              rows={3}
+              rows={8}
+              autoFocus={true}
+              onKeyDown={(event) => {
+                if (event.key === "Escape") {
+                   !!onClose && onClose();
+                   dispatch(resetEditorText());
+                }
+             }}
             />
-        </Grid>
-
-        <Grid
-          className={classes.actionContainer}
-          container
-          justify="space-between"
-        >
-          <IconButton variant="outlined" type="submit" size="small">
-            <SaveIcon />
-          </IconButton>
-          <IconButton variant="outlined" onClick={() => onClose()} size="small">
-            <CancelIcon />
-          </IconButton>
         </Grid>
       </form>
     </>
@@ -108,6 +101,11 @@ const useStyles = makeStyles((theme) => ({
   },
   formInput: {
     marginBottom: theme.spacing(1),
+    
+    "& .MuiOutlinedInput-multiline": {
+      padding: 5,
+      fontSize: 12
+    }
   },
   actionContainer: {
     marginTop: theme.spacing(1),
