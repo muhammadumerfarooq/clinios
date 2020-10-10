d06bc3210cdeb6ef3b2193c6dea88e07b2951349
Ruhul Amin <r_amin105@yahoo.com>
2020-10-10 08:35:50 +0200

    Add import order eslint

--- client/src/screens/patient-portal/Home/Home.js
+++ client/src/screens/patient-portal/Home/Home.js
@@ -1,19 +1,21 @@
 import React, { useEffect } from "react";
+
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import { useHistory, useParams } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import clsx from "clsx";
-import AuthService from "./../../../services/patient_portal/auth.service";
-import { AuthConsumer } from "./../../../providers/AuthProvider";
-import { loginComplete } from "./../../../store/auth/actions";
+import { useDispatch } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+
 import Error from "./../../../components/common/Error";
+import { AuthConsumer } from "./../../../providers/AuthProvider";
+import AuthService from "./../../../services/patient_portal/auth.service";
+import { loginComplete } from "./../../../store/auth/actions";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -54,7 +56,6 @@ const Home = () => {
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
-
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon className={classes.lockIcon} />
