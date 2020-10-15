467f7e4385f7c1a07e6b99690735b14f9c909b54
Arslan Sajid <arslansajid951@gmail.com>
2020-10-15 10:26:52 +0500

    Added tabs theme override

---
+++ client/src/theme/overrides/MuiTab.js
@@ -0,0 +1,12 @@
+export default {
+  root: {
+    "&.MuiTab-root": {
+      minWidth: 75,
+      fontSize: 14,
+    },
+    "&.Mui-selected": {
+      borderBottom: "2px solid #2979FF",
+      borderColor: "#2979FF",
+    }
+  }
+};
